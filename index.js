bb37e3dbf293f3e37710566e77e694690965b8bc
Iris Bromley <iris.j.bromley@gmail.com>
2023-01-10 21:21:36 +0100

    It works now!

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-console.log('#');
+import chalk from 'chalk';
+import randomColor from 'randomcolor'; // import the script
 
-// block of approximately 31x9 # characters
+const color = randomColor();
+
+function logWithColor(value) {
+  console.log(chalk.hex(color)(value));
+}
+
+// get a 31x3 block of # character
+for (let i = 0; i < 3; i++) {
+  logWithColor('#'.repeat(31));
+}
+
+logWithColor('#'.repeat(5) + ' '.repeat(21) + '#'.repeat(5));
+logWithColor(
+  '#'.repeat(5) + ' '.repeat(7) + color + ' '.repeat(7) + '#'.repeat(5),
+);
+logWithColor('#'.repeat(5) + ' '.repeat(21) + '#'.repeat(5));
+for (let i = 0; i < 3; i++) {
+  logWithColor('#'.repeat(31));
+}
+
+logWithColor(color);
